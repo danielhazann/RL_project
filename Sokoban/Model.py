310c91053c42dc050413f8779dce4f74ba57548c
Daniel <49162653+danielhazann@users.noreply.github.com>
2021-08-22 21:22:24 +0300

    Update Model.py

--- Sokoban/Model.py
+++ Sokoban/Model.py
@@ -6,8 +6,6 @@ class Net(nn.Module):
 
     def __init__(self, state_size, action_size, seed):
         super(Net, self).__init__()
-        # self.seed = torch.manual_seed(seed)
-        # self.pad = nn.ZeroPad2d(1)
         self.fc1 = nn.Linear(state_size, 256)
         self.fc2 = nn.Linear(256, 128)
         self.fc3 = nn.Linear(128, action_size)
